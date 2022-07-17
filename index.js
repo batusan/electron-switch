64b2f8469f25dfc1e87f28049a1119a50c073dbc
Batuhan San <batuhansann@gmail.com>
2022-07-18 02:27:24 +0300

    UI / Switch functions done. also Added mp3

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow,Menu } = require("electron");
 const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 700,
+    resizable: false,
+    icon: path.join(__dirname,"blockchain.png"),
+    //transparent:true,
+    //frame:false,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
 
   win.loadFile("index.html");
-  win.webContents.on("before-input-event", (event, input) => {
-    console.log(input);
-  });
+  //win.webContents.openDevTools(); // Dev menu
+  Menu.setApplicationMenu(null)
 };
 
 app.whenReady().then(() => {
--- preload.js
+++ preload.js
@@ -1,29 +1,76 @@
+/*
+  TODO 
+  -MOUSE SUPPORT (inş)
+*/
 const { exec } = require("child_process");
 const { uIOhook, UiohookKey } = require("uiohook-napi");
 
-let backgroundColor;
+let isSwitching = false; // Secure timeout
+let delay = 3000; //ms
+let switchKey = UiohookKey.Q; // Default Q
+
+let sound = new Audio("mc.mp3"); // nam nam sound
 
 uIOhook.on("keydown", (e) => {
-  if (e.keycode === UiohookKey.Q) {
-    timeout = setTimeout(alertFunc, 3000);
-    backgroundColor.style.backgroundColor = "black";
+  if (!isSwitching) {
+    if (e.keycode === switchKey) {
+      switchIt();
+      timeout = setTimeout(switchIt, delay);
+    }
   }
 });
 
-uIOhook.on("keyup", (e) => {
-  if (e.keycode === UiohookKey.Q) {
-    backgroundColor.style.backgroundColor = "green";
+const switchIt = () => {
+  if (!isSwitching) {
+    sound.play();
+    execute(
+      "netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound",
+      (output) => {
+        console.log(output);
+      }
+    );
+  } else {
+    execute(
+      "netsh advfirewall set allprofiles firewallpolicy blockinbound,allowoutbound",
+      (output) => {
+        console.log(output);
+      }
+    );
   }
-});
-
-const colorChanger = () =>{
-    backgroundColor.style.backgroundColor.togg
-}
+  isSwitching = !isSwitching;
+};
 
 window.addEventListener("DOMContentLoaded", () => {
-  backgroundColor = document.getElementById('bg');
-  const buttonTest = document.getElementById("test");
-  buttonTest.addEventListener("click", () => {});
+  let saveBtn = document.querySelector("#btn_save");
+  let keys = document.querySelector("#keys");
+  let msDelay = document.querySelector("#msdelay");
+
+  //Save Key
+  saveBtn.addEventListener("click", () => {
+    if (msDelay.value > 0) {
+      delay = msDelay.value;
+    } else {
+      alert("set ms delay to higher than 0");
+    }
+    switchKey = parseInt(keys.value);
+  });
+
+  //Insert keys to select input
+  if (UiohookKey) {
+    for (key in UiohookKey) {
+      var opt = document.createElement("option");
+      opt.value = UiohookKey[key];
+      opt.innerHTML = key;
+      keys.appendChild(opt);
+    }
+  }
 });
+
+//execute powershell commands from this func
+function execute(command, callback) {
+  exec(command, (error, stdout, stderr) => {
+    callback(stdout);
+  });
+}
 
 uIOhook.start();
